1b132784052f198dee95a86180ffb950637c2435
Maria Polak <maria.polak@spectral-plugins.com>
2023-09-21 14:12:24 +0200

    Class 2 Exercises
    -Bugged (featured) Flanger

--- Meeting 2/Class.m
+++ Meeting 2/Class.m
@@ -45,6 +45,53 @@ end
 % plot(synth);
 soundsc(synth,Fs);
 
+%% FLANGER | CHORUS
+
+delayBaseMs = 30; %ms
+delayBase = round(delayBaseMs / 1000 * Fs);
+
+freqLFO = 1; % Hz
+lfo = sin(2*pi*((1:(length(x)+delayBase))*freqLFO/Fs));
+
+flangerMaxDelayMs = 5; % ms
+flangerMaxDepth = round(flangerMaxDelayMs / 1000 * Fs);
+
+flangerOut = zeros(length(x) + delayBase, 2);
+xPadded = [x;zeros(delayBase,2)];
+
+circularBuffer = zeros(delayBase + flangerMaxDepth,2);
+read = 1;
+write = delayBaseMs;
+
+for i = 1:length(flangerOut)
+    lfoRead = round(lfo(i)*flangerMaxDepth);
+    tmpRead = read + lfoRead;
+    
+    if tmpRead <= 0
+        tmpRead = length(circularBuffer) + tmpRead;
+    end
+    if tmpRead > length(circularBuffer)
+        tmpRead = tmpRead - length(circularBuffer);
+    end
+
+    flangerOut(i,1) = circularBuffer(tmpRead, 1);
+    flangerOut(i,2) = circularBuffer(tmpRead, 2);
+
+    circularBuffer(write, 1) = xPadded(i, 1);
+    circularBuffer(write, 2) = xPadded(i, 2);
+
+    write = write + 1;
+    read = read + 1;
+
+    if write > length(circularBuffer)
+        write = 1;
+    end
+    if read > length(circularBuffer)
+        read = read - length(circularBuffer);
+    end
+end
+
+plot(flangerOut);
 
 
 %% FUNCTIONS
