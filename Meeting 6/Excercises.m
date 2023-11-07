fd8e31b34e631071473aa4585e2b28a52bb15809
Maria Polak <maria.polak@spectral-plugins.com>
2023-11-07 16:45:22 +0100

    Excercises 6

--- Meeting 6/Excercises.m
+++ Meeting 6/Excercises.m
@@ -29,4 +29,37 @@ plot(y);
 
 %% FRACTIONAL DELAY
 
-%% ALL PASS
+y1 = zeros(Len, 1);
+delay = 100.227;
+fracL = delay - fix(delay);
+intL = fix(delay);
+
+
+% y[n] = x[n] + 0.5y[n − L] + 0.5y[n − (L + 1)]
+for n = 1:Len
+    y1(n) = x(n); % + x[n]
+    if n - L > 0
+        y1(n) = y1(n) + 0.5 * fracDelay(y1, n - intL, fracL); %  + 0.5y[n − L]
+    end
+    if n - (L+1) > 0
+        y1(n) = y1(n) + 0.5 * fracDelay(y1, n - intL - 1, fracL); % + 0.5y[n − (L + 1)] 
+    end
+end
+
+plot(y1);
+
+%% PLOTING
+
+subplot(3,1,1);
+plot(y);
+subplot(3,1,2);
+plot(y1);
+subplot(3,1,3);
+plot(y1-y);
+
+
+%% functions
+
+function x = fracDelay(y, i, frac)
+    x = (1 - frac) * y(i) + frac * y(i+1);
+end
